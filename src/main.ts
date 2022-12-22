ebb1f36e11d0e054d6f970880a68f96bcdde3a7f
Yogesh Kumar <77193839+Yogesh011-R@users.noreply.github.com>
2022-12-22 11:16:04 +0530

    init

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-if (process.env.NODE_ENV === "local") {
-  let config = require("dotenv").config;
-  config();
-}
-import express from "express";
-import helmet from "helmet";
-import { appIndividual } from "./individual";
-import { Database } from "./shared/database";
-import { loggerRoot } from "./shared/logger";
-import cors from "cors";
-import path from "path";
-import job from "./cron";
+let config = require('dotenv').config;
+config();
 
-let uri: string = process.env.DB_URL ?? "";
+import express from 'express';
+import helmet from 'helmet';
+import { appIndividual } from './individual';
+import { Database } from './shared/database';
+import { loggerRoot } from './shared/logger';
+import cors from 'cors';
+import path from 'path';
+import job from './cron';
+
+let uri: string = process.env.DB_URL ?? '';
 
 const app = express();
 
@@ -20,25 +19,25 @@ const database = new Database();
 database
   .connect()
   .then(() => {
-    console.log("Connected");
+    console.log('Connected');
   })
-  .catch((err) => {
+  .catch(err => {
     loggerRoot.error(err.message);
     process.exit(0);
   });
 
 app.use(cors());
 app.use(helmet({ contentSecurityPolicy: false }));
-app.use("/api/", appIndividual);
-app.use(express.static(path.join(__dirname, "../../frontend-build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../frontend-build", "index.html"));
+app.use('/api/', appIndividual);
+app.use(express.static(path.join(__dirname, '../../frontend-build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend-build', 'index.html'));
 });
 
 // 404 handler
 app.use((req, res) => {
   res.status(404).json({
-    message: "404 path not found",
+    message: '404 path not found',
     data: {
       method: req.method,
       path: req.originalUrl,
@@ -50,7 +49,7 @@ app.use((err: any, req: any, res: any, next: any) => {
   if (res.headersSent) return next(err);
   console.log(err);
   res.status(500).json({
-    message: "500 server error",
+    message: '500 server error',
     data: {
       method: req.method,
       path: req.originalUrl,
